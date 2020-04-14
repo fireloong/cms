d8860cc1ea275579cf72dcec5dc1b02ada0ab82b
FireLoong <fireloong@foxmail.com>
2020-04-14 16:48:48 +0800

    update doc

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -1,10 +1,10 @@
 $(function(){
   $('.iconfont.icon-caret-right').on('click', function(){
     $(this).toggleClass('icon-caret-right').toggleClass('icon-sort-down');
-    $('#aa').collapse('show');
+    $('#aa').collapse('toggle');
   });
   $('.iconfont.icon-sort-down').on('click', function(){
     $(this).toggleClass('icon-caret-right').toggleClass('icon-sort-down');
-    $('#aa').collapse('hide');
+    $('#aa').collapse('toggle');
   });
 });
